91e830ea3ef320a9b9a25458850649fbd5aa213c
brandonn <brandon7.7porcel@gmail.com>
2021-07-08 17:19:49 -0300

    subiendo ejercicios,abm

---
+++ ejercicios.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ejercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Int16> listNumeros = new List<Int16>();
+
+
+            listNumeros.Add(20);
+            listNumeros.Add(2);
+            listNumeros.Add(30);
+            listNumeros.Add(10);
+            listNumeros.Add(5);
+
+            int posDiez = listNumeros.IndexOf(10);
+            listNumeros.Insert(posDiez + 1, 0);
+            listNumeros.Insert(posDiez - 1, 0);
+            //4
+            listNumeros.RemoveAt(0);
+            listNumeros.RemoveAt(listNumeros.Count - 1);
+            //5
+            listNumeros.RemoveAt(1);
+            listNumeros.RemoveAt(listNumeros.Count - 2);
+            Console.ReadKey();
+
+        }
+    }
+}
